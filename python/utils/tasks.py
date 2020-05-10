bf7a7bbb209124d636817a182d717ef4c7f915c3
pultarmi <milan.pultar@gmail.com>
2020-05-11 00:24:35 +0200

    fast push

--- python/utils/tasks.py
+++ python/utils/tasks.py
@@ -211,7 +211,7 @@ def eval_matching(descr,split):
     pbar = tqdm(split['test'], desc=green('matching'))
     for seq in pbar:
         d_ref = getattr(descr[seq], 'ref')
-        # print(d_ref.shape)
+        print(d_ref.shape)
         gt_l = np.arange(d_ref.shape[0])
         for t in tp:
             for i in range(1,6):
